6c4a74863be7d8f8c5b83a89158ca6193463a8a6
ketan-2905 <ketangaikwad2905@gmail.com>
2025-09-22 10:18:01 +0530

    Minor tweaks

--- src/lib/axiosClient.ts
+++ src/lib/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "https://rockfallprediction-git-main-ketan-2905s-projects.vercel.app/api",
+  baseURL: "https://rockfallprediction-five.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
